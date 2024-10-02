3edc5444c3debc51b61ef4d08bdb997906a9882c
Bilalkhankm2 <bilalahmadkm@gmail.com>
2024-10-03 00:42:53 +0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,15 @@
+# # Enter string type through user
+name = input("Enter your name here =")
+print("name =",name)
+#
+# # Enter integer type through user
+age = int(input("Enter your age here ="))
+print("age =",age)
+#
+# # Enter float type through user
+length = float(input("Enter the length of the wall ="))
+print("length =",length)
+
+# Enter evaluate type through user
+expression = eval(input("Enter the number of evaluate ="))
+print("Evaluate operation =",expression)
