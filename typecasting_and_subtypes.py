a17b2eec3f3133bf3b2148d887df941ad0089555
Bilalkhankm2 <bilalahmadkm@gmail.com>
2024-10-03 01:09:45 +0500

    Add files via upload

---
+++ typecasting_and_subtypes.py
@@ -0,0 +1,21 @@
+# check datatype string
+name = "bilal"
+print(type(name))
+
+# check datatype integer
+age = 15
+print(type(age))
+
+# check datatype float
+length = 27.8
+print(type(length))
+
+
+a =4
+b =8.6
+print(type(a))
+print(type(b))
+c =a+b
+c = str(c)
+print("after conversion =",type(c))
+print(type(c))
